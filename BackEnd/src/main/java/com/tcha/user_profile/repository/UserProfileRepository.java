330d45fb268916dbcffbb64f02cfd236ec699a8e
Ha Jung Ho <ggk14@naver.com>
2023-07-21 17:56:37 +0900

    save: user_profile 구현 임시 저장

--- BackEnd/src/main/java/com/tcha/user_profile/repository/UserProfileRepository.java
+++ BackEnd/src/main/java/com/tcha/user_profile/repository/UserProfileRepository.java
@@ -1,5 +1,8 @@
 package com.tcha.user_profile.repository;
 
-public interface UserProfileRepository {
+import com.tcha.user_profile.entity.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserProfileRepository extends JpaRepository<UserProfile, String> {
 
 }

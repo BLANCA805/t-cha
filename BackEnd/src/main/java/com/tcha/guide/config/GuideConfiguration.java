4110adcca5c3184875007a54f3fd344ca4931340
chech2 <chaelim3396@gmail.com>
2023-07-21 17:53:39 +0900

    save: guide CRUD 기능 구현

---
+++ BackEnd/src/main/java/com/tcha/guide/config/GuideConfiguration.java
@@ -0,0 +1,17 @@
+package com.tcha.guide.config;
+
+import com.tcha.guide.dto.GuideDto;
+import com.tcha.guide.entity.Guide;
+import org.springframework.context.annotation.Bean;
+
+public class GuideConfiguration {
+    @Bean
+    public Guide guide(){
+        return new Guide();
+    }
+    @Bean
+    public GuideDto guideDto(){
+        return new GuideDto();
+    }
+
+}
---
+++ BackEnd/src/main/java/com/tcha/guide/mapper/GuideMapper.java
@@ -0,0 +1,15 @@
+package com.tcha.guide.mapper;
+
+import com.tcha.guide.dto.GuideDto;
+import com.tcha.guide.entity.Guide;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+
+@Mapper(componentModel = "spring")
+public interface GuideMapper {
+    Guide guidePostDtoToGuide(GuideDto.post requestBody);
+    Guide guidePatchDtoToGuide(GuideDto.patch requestBody);
+    GuideDto.Response guideToGuideResponse(Guide guide);
+
+}

636ac1844a40958fb7239afba2877e8290862c0b
Ha Jung Ho <ggk14@naver.com>
2023-07-21 17:56:25 +0900

    save: user 구현 임시 저장

--- BackEnd/src/main/java/com/tcha/user/repository/UserRepository.java
+++ BackEnd/src/main/java/com/tcha/user/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.tcha.user.repository;
 import com.tcha.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
 
 }
 

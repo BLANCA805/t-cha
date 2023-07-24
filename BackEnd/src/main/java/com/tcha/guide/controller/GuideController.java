1ff2354b8395005ae6dd23001336fce17de0bc4e
chech2 <chaelim3396@gmail.com>
2023-07-24 21:34:57 +0900

    save: guide 컨벤션 맞추기 & 필요없는 파일 삭제 중간 저장

--- BackEnd/src/main/java/com/tcha/guide/controller/GuideController.java
+++ BackEnd/src/main/java/com/tcha/guide/controller/GuideController.java
@@ -4,11 +4,8 @@ import com.tcha.guide.dto.GuideDto;
 import com.tcha.guide.entity.Guide;
 import com.tcha.guide.mapper.GuideMapper;
 import com.tcha.guide.service.GuideService;
-import com.tcha.user.dto.UserDto;
-import com.tcha.user.entity.User;
 
 import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -16,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/guide")
+@RequestMapping("/guides")
 @Slf4j
 @RequiredArgsConstructor //생성자 주입, final 변수 주입
 @RestControllerAdvice
@@ -28,7 +25,7 @@ public class GuideController {
 
     //아직 기능 코드에 대한 조건 없음, 추후 유효성 검사 로직 추가 예정
     @PostMapping
-    public ResponseEntity postGuide(@RequestBody GuideDto.post postRequest){
+    public ResponseEntity postGuide(@RequestBody GuideDto.post postRequest) {
         Guide postGuide = guideMapper.guidePostDtoToGuide(postRequest);
         Guide guideForResponse = guideService.createGuide(postGuide);
         GuideDto.Response response = guideMapper.guideToGuideResponse(guideForResponse);
@@ -37,7 +34,7 @@ public class GuideController {
 
     //기능 코드별 사용 가이드 보기
     @GetMapping("/{code}")
-    public ResponseEntity getGuide(@PathVariable("code") String code) throws Exception{
+    public ResponseEntity getGuide(@PathVariable("code") String code) throws Exception {
         List<GuideDto.Response> response = guideService.findAllCodeGuide(code);
         return new ResponseEntity(response, HttpStatus.CREATED);
 
@@ -45,7 +42,7 @@ public class GuideController {
 
     //1개의 사용 가이드 확인
     @GetMapping("/code/{id}")
-    public ResponseEntity getOneGuide(@PathVariable("id") Long id) throws Exception{
+    public ResponseEntity getOneGuide(@PathVariable("id") Long id) throws Exception {
         GuideDto.Response response = guideService.findOneGuide(id);
 
         return new ResponseEntity(response, HttpStatus.CREATED);
@@ -53,10 +50,9 @@ public class GuideController {
 
     //서비스 가이드 수정
     @PatchMapping
-    public ResponseEntity patchGuide(@RequestBody GuideDto.patch patchRequest){
+    public ResponseEntity patchGuide(@RequestBody GuideDto.patch patchRequest) {
         Guide patchGuide = guideMapper.guidePatchDtoToGuide(patchRequest);
         int response = guideService.patchGuide(patchGuide);
-//        GuideDto.Response response = guideMapper.guideToGuideResponse(guideForResponse);
         return new ResponseEntity(response, HttpStatus.CREATED);
     }
 

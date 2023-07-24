8b6131161a9d03857c3ed0a0848d8faece9a4059
Ha Jung Ho <ggk14@naver.com>
2023-07-25 00:39:08 +0900

    feat: spring security를 적용한 로그인 및 회원가입 구현

---
+++ BackEnd/src/main/java/com/tcha/auth/exception/ErrorResponder.java
@@ -0,0 +1,18 @@
+package com.tcha.auth.exception;
+
+import com.nimbusds.jose.shaded.gson.Gson;
+import com.tcha.utils.exceptions.controller.ErrorResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+public class ErrorResponder {
+    public static void sendErrorResponse(HttpServletResponse response, HttpStatus status) throws IOException {
+        Gson gson = new Gson();
+        ErrorResponse errorResponse = ErrorResponse.of(status);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(status.value());
+        response.getWriter().write(gson.toJson(errorResponse, ErrorResponse.class)); // 출력 스트림(response)
+    }
+}

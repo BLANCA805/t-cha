9b7974ade310b8e425053dea0d262ab04074bd64
chech2 <chaelim3396@gmail.com>
2023-07-28 05:34:07 +0900

    save: STOMP 프로토콜 적용 중

--- BackEnd/src/main/java/com/tcha/chatting/dto/Message.java
+++ BackEnd/src/main/java/com/tcha/chatting/dto/Message.java
@@ -1,23 +1,28 @@
-//package com.tcha.chatting.dto;
-//
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.Id;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//@Getter
-//@Setter
-//@Entity
-//public class Message {
-//
-//    @Id
-//    private String id;
-//    private MessageType type;
-//    private String sender; // 보낸 사람
-//    private String content; // 메세지 내용
-//
-//
-//    public enum MessageType {
-//        ENTER, TALK;
-//    }
-//}
+package com.tcha.chatting.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Message {
+
+    @Id
+    private String channelId;
+    private MessageType type;
+    private String sender; // 보낸 사람
+    private Object data; // 메세지 내용
+
+
+    public enum MessageType {
+        CONNECT, TALK, DISCONNECT;
+    }
+}

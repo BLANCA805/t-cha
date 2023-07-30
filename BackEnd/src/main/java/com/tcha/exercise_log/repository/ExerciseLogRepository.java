4801469c32751d8852e83af0908ab06115301474
변정원 <labhg64@gmail.com>
2023-07-31 03:39:35 +0900

    fix: exercise-log 이미지 업로드 구현 중...

--- BackEnd/src/main/java/com/tcha/exercise_log/repository/ExerciseLogRepository.java
+++ BackEnd/src/main/java/com/tcha/exercise_log/repository/ExerciseLogRepository.java
@@ -1,9 +1,23 @@
 package com.tcha.exercise_log.repository;
 
 import com.tcha.exercise_log.entity.ExerciseLog;
+import com.tcha.review.entity.Review;
+import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ExerciseLogRepository extends JpaRepository<ExerciseLog, Long> {
 //    ExerciseLog findByLiveId(Long id);
+
+//    @Query("SELECT e FROM ExerciseLog e WHERE e.trainer.id = :trainer_id")
+//    Page<Review> findAllByTrainerId(@Param("trainer_id") UUID trainerId, Pageable pageable);
+//
+//    @Query("SELECT e FROM ExerciseLog e WHERE e.userProfile.id = :user_profile_id")
+//    Page<Review> findAllByUserProfileId(@Param("user_profile_id") Long userProfileId,
+//            Pageable pageable);
+
 
 }

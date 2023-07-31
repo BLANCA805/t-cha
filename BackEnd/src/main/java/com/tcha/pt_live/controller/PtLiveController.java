6cd392fbe879552e9c1c39aefcafcdbceea74001
chogoal <goal961@naver.com>
2023-07-31 15:40:42 +0900

    feat: pt_live CRUD 완성

--- BackEnd/src/main/java/com/tcha/pt_live/controller/PtLiveController.java
+++ BackEnd/src/main/java/com/tcha/pt_live/controller/PtLiveController.java
@@ -1,9 +1,17 @@
 package com.tcha.pt_live.controller;
 
+import com.tcha.pt_live.dto.PtLiveDto;
 import com.tcha.pt_live.service.PtLiveService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +23,63 @@ import org.springframework.web.bind.annotation.RestController;
 public class PtLiveController {
 
     private final PtLiveService ptLiveService;
+
+    /*
+    유저가 PT 수업 예약
+    결제 성공 시 live row 생성
+     */
+    @PostMapping("/{class-id}/{user-profile-id}")
+    public ResponseEntity<PtLiveDto.Response> postPtLive(
+            @PathVariable("class-id") long ptClassId,
+            @PathVariable("user-profile-id") long userProfileId) {
+
+        log.debug("[PtLiveController] postPtLive 접근 확인 ::: ptClassId = {}, userProfileId = {}",
+                ptClassId, userProfileId);
+
+        // 결제 로직 추가
+
+        ptLiveService.createPtLive(ptClassId, userProfileId); // 리턴 값(프론트에 전달할 값) 추가
+
+        return ResponseEntity.ok().body(null);
+    }
+
+    /*
+    live 정보 수정
+    - 언제?
+        - 유저 또는 트레이너가 예약 취소했을 경우
+        - 연결된 userProfile이 null로 변경됨
+     */
+    @PatchMapping("/{pt-live-id}/{user-profile-id}")
+    public ResponseEntity<PtLiveDto.Response> patchPtLive(
+            @PathVariable("pt-live-id") long ptLiveId,
+            @PathVariable("user-profile-id") long userProfileId) {
+
+        ptLiveService.updatePtLive(ptLiveId, userProfileId);
+
+        return ResponseEntity.ok().body(null);
+    }
+
+    /*
+    pt live id를 통해 라이브 정보 조회
+     */
+    @GetMapping("/{pt-live-id}")
+    public ResponseEntity<PtLiveDto.Response> getOnePtLive(
+            @PathVariable("pt-live-id") long ptLiveId) {
+
+        PtLiveDto.Response ptLive = ptLiveService.findOnePtLive(ptLiveId);
+
+        return ResponseEntity.ok().body(ptLive);
+    }
+    
+    /*
+    pt live id를 통해 등록된 pt 라이브 삭제
+     */
+    @DeleteMapping("{pt-live-id}")
+    public ResponseEntity<?> deletePtLive(@PathVariable("pt-live-id") long ptLiveId) {
+
+        ptLiveService.deletePtLive(ptLiveId);
+
+        return ResponseEntity.ok().body(null);
+    }
 
 }

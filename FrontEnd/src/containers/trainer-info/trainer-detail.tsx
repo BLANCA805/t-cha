8fb143f24f894d16d09c95e98f834f759f7a406a
ikthegreat <ikthegreat@icloud.com>
2023-08-03 13:25:44 +0900

    feat: Trainer API 연결 완료

--- FrontEnd/src/containers/trainer-info/trainer-detail.tsx
+++ FrontEnd/src/containers/trainer-info/trainer-detail.tsx
@@ -1,26 +1,19 @@
+import React from "react";
+import { TrainerDetailData } from "src/interface";
 import styled from "styled-components";
-
-export interface detailData {
-  data: {
-    id: string;
-    introduction: string;
-    tags: string;
-    title: string;
-    content: string;
-    images: Array<string>;
-    profileImg: string;
-    profileName: string;
-  };
-}
 
 const Wrapper = styled.div`
   margin: 1%;
   padding: 3%;
 `;
 
-function TrainerDetail() {
-  console.log();
-  return <Wrapper>트레이너 상세 정보 페이지 입니다.</Wrapper>;
+interface TrainerDetailDataProps {
+  data: TrainerDetailData;
 }
+
+const TrainerDetail: React.FC<TrainerDetailDataProps> = ({ data }) => {
+  console.log(data);
+  return <Wrapper>트레이너 상세 정보 페이지 입니다.</Wrapper>;
+};
 
 export default TrainerDetail;

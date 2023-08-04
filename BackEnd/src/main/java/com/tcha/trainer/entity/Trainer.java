5d063bdd6d4e27c9db07638afb35765cca52bdba
Ha Jung Ho <ggk14@naver.com>
2023-08-04 17:37:20 +0900

    feat: trainerID type 변경(uuid -> String)으로 인한 수정

--- BackEnd/src/main/java/com/tcha/trainer/entity/Trainer.java
+++ BackEnd/src/main/java/com/tcha/trainer/entity/Trainer.java
@@ -34,8 +34,8 @@ public class Trainer extends Auditable {
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name = "uuid2", strategy = "uuid2")
 //    @Column(name = "TRAINER_ID", columnDefinition = "BINARY(16)")
-    @Column(columnDefinition = "BINARY(16)")
-    private UUID id;
+    @Column(columnDefinition = "CHAR(36)")
+    private String id;
 
     @OneToOne
     @JoinColumn(name = "USER_PROFILE_ID")

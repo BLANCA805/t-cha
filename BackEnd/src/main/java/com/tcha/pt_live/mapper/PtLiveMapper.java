6f3a8737e90d7514ac9b389a0fdbeef68a2d81d2
chogoal <goal961@naver.com>
2023-08-05 18:21:09 +0900

    feat: pt_live CRUD legacy 완성

--- BackEnd/src/main/java/com/tcha/pt_live/mapper/PtLiveMapper.java
+++ BackEnd/src/main/java/com/tcha/pt_live/mapper/PtLiveMapper.java
@@ -1,6 +1,6 @@
 package com.tcha.pt_live.mapper;
 
-
+import com.tcha.pt_class.entity.PtClass;
 import com.tcha.pt_live.dto.PtLiveDto;
 import com.tcha.pt_live.entity.PtLive;
 import org.mapstruct.Mapper;
@@ -8,14 +8,17 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface PtLiveMapper {
 
-    default PtLiveDto.Response ptLiveToResponseDto(PtLive ptLive) {
+    default PtLiveDto.Response ptLiveToResponseDto(PtLive ptLive, PtClass ptClass) {
         return PtLiveDto.Response.builder()
                 .ptLiveId(ptLive.getId())
-//                .trainerProfileImage()
-//                .trainerName()
-//                .startAt()
-//                .closeAt()
+                .ptClassId(ptClass.getId())
+                .startAt(ptClass.getStartAt())
+                .closeAt(ptClass.getCloseAt())
+//                .price(ptClass.getPrice())
                 .price("7,000원")
+                .trainerId(ptLive.getTrainerId())
+                .trainerProfileImage(ptClass.getTrainer().getUserProfile().getProfileImage())
+                .trainerName(ptClass.getTrainer().getUserProfile().getName())
                 .build();
     }
 

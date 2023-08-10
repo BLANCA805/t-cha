02c7341e9a2e8a94ad821f5cef3d1047a0462771
Ha Jung Ho <ggk14@naver.com>
2023-08-11 00:28:49 +0900

    feat: Spring Security 권한 필터 추가

--- BackEnd/src/main/java/com/tcha/auth/config/SecurityConfiguration.java
+++ BackEnd/src/main/java/com/tcha/auth/config/SecurityConfiguration.java
@@ -56,8 +56,6 @@ public class SecurityConfiguration {
                 .sessionManagement().sessionCreationPolicy(
                         SessionCreationPolicy.STATELESS)// 세션 정책 : 세션을 생성하지 않도록 설정 => JWT 사용 환경에선 세션을 사용하지 않음(SecurityContext 정보를 얻을 때 Session 을 사용하지 않음)
                 .and()
-//                .requiresChannel((requiresChannel) -> requiresChannel.requestMatchers(
-//                        "/oauth2/authorization/google").requiresSecure())
                 .formLogin().disable()
                 .httpBasic().disable()
                 .exceptionHandling()
@@ -69,6 +67,47 @@ public class SecurityConfiguration {
                 .apply(new CustomFilterConfigurer()) // 커스텀 필터 추가
                 .and()
                 .authorizeHttpRequests(authorize -> authorize
+                                //Answer
+                                .requestMatchers(HttpMethod.POST, "/answers").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/answers/**").permitAll()
+                                .requestMatchers(HttpMethod.DELETE, "/answers/**").hasRole("ADMIN")
+                                //Bookmark
+                                .requestMatchers(HttpMethod.POST, "/bookmarks/**").hasRole("USER")
+                                .requestMatchers(HttpMethod.DELETE, "/bookmarks/*").hasRole("USER")
+                                .requestMatchers(HttpMethod.DELETE, "/bookmarks/**").hasRole("USER")
+                                .requestMatchers(HttpMethod.GET, "/bookmarks/*").hasRole("USER")
+                                .requestMatchers(HttpMethod.GET, "/bookmarks/**").hasRole("USER")
+                                //ExerciseLog
+                                .requestMatchers(HttpMethod.POST, "/exercise-logs/*").hasRole("TRAINER")
+                                .requestMatchers(HttpMethod.PATCH, "/exercise-logs/*").hasRole("TRAINER")
+                                .requestMatchers(HttpMethod.GET, "/exercise-logs/*").hasAnyRole("USER")
+                                .requestMatchers(HttpMethod.GET, "/exercise-logs").hasAnyRole("USER")
+                                .requestMatchers(HttpMethod.GET, "/exercise-logs/ptLive/*").hasRole("TRAINER")
+                                //Guide
+                                .requestMatchers(HttpMethod.POST, "/guides").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/guides/*").hasAnyRole("USER")
+                                .requestMatchers(HttpMethod.GET, "/guides/**").hasAnyRole("USER")
+                                .requestMatchers(HttpMethod.PATCH, "/guides/*").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.DELETE, "/guides/*").hasRole("ADMIN")
+                                //Notice
+                                .requestMatchers(HttpMethod.POST, "/notices").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/notices/*").hasAnyRole("USER")
+                                .requestMatchers(HttpMethod.GET, "/notices/**").hasAnyRole("USER")
+                                .requestMatchers(HttpMethod.PATCH, "/notices/*").hasAnyRole("ADMIN")
+                                .requestMatchers(HttpMethod.DELETE, "/notices/*").hasAnyRole("ADMIN")
+                                //Class
+                                .requestMatchers(HttpMethod.POST, "/classes").hasRole("TRAINER")
+                                .requestMatchers(HttpMethod.GET, "/classes/*").hasAnyRole("USER")
+                                .requestMatchers(HttpMethod.GET, "/classes/**").hasAnyRole("USER")
+                                .requestMatchers(HttpMethod.PATCH, "/classes").hasAnyRole("USER")
+                                .requestMatchers(HttpMethod.DELETE, "/classes/*").hasAnyRole("TRAINER")
+                                //Question
+                                .requestMatchers(HttpMethod.POST, "/questions").hasRole("USER")
+                                .requestMatchers(HttpMethod.GET, "/questions/*").hasAnyRole("USER")
+                                .requestMatchers(HttpMethod.GET, "/questions/**").hasAnyRole("USER")
+                                .requestMatchers(HttpMethod.DELETE, "/questions/*").hasAnyRole("USER")
+                                //Trainer
+
 //                                .requestMatchers( HttpMethod.POST, "/users").permitAll()
 //                                .requestMatchers( "/users/**").hasAnyRole("USER")
 //                                .requestMatchers( "/userProfiles/**").hasAnyRole("USER")

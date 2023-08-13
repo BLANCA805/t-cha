4606de6c09cd2c526a423f48843d4e441b5236ea
chogoal <goal961@naver.com>
2023-08-14 00:54:47 +0900

    save: 오픈비두 구현 중

--- FrontEnd/src/hooks/use-openvidu.ts
+++ FrontEnd/src/hooks/use-openvidu.ts
@@ -1,23 +1,64 @@
-import { OpenVidu } from "openvidu-browser";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { type RootState } from "../redux/store";
+import { api } from "@shared/common-data";
+import { Session, OpenVidu, StreamManager } from "openvidu-browser";
 
 import axios from "axios";
 
 const OPENVIDU_SERVER_URL = "https://www.tcha.site:8443/";
 const OPENVIDU_SERVER_SECRET = "blanca05";
 
+export interface PtLiveData {
+  liveId: number;
+  ptudentId: string; // 회원(유저) 아이디
+  trainerId: string; // 트레이너 아이디
+  status: string; // pt 라이브 상태 (진행 중, 종료 가능, 종료)
+}
+
+// 예약 성공 시 라이브 생성 (patchPtClass)
+export function createPtLive(ptLiveId: number) {
+  const OV = new OpenVidu();
+  const ptLive = OV.initSession();
+
+  
+
+  console.log("createPtLive : " + ptLive.sessionId)
+
+  return ptLive;
+}
+
+// 라이브 입장 (pt 라이브 아이디를 통해)
+export function EnterPtLive(ptLiveId: number) {
+
+  // 현재 로그인한 유저의 프로필 아이디 가져오기 (jwt token으로 수정하기)
+  const profileId = useSelector((state: RootState) => state.profile.profileId);
+  const [ptLive, setPtLiveData] = useState<PtLiveData>();
+
+  // 라이브 방 정보
+  const ptLiveInfo = () => {
+    axios
+      .get(`${api}/lives/${ptLiveId}`)
+      .then((response) => {
+        setPtLiveData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+}
+
 export function getToken(liveId: string): Promise<any> {
   return createSession(liveId).then((liveId) => createToken(liveId));
 }
 
-export function createSession(sessionId: string): Promise<any> {
-  const data = JSON.stringify({ customSessionId: sessionId });
+function createSession(liveId: string): Promise<any> {
+  const data = JSON.stringify({ customSessionId: liveId });
   return new Promise((resolve, reject) => {
     axios
       .post(OPENVIDU_SERVER_URL + "openvidu/api/sessions", data, {
         headers: {
-          Authorization:
-            "Basic " + btoa("OPENVIDUAPP:" + OPENVIDU_SERVER_SECRET),
+          Authorization: "Basic " + btoa("OPENVIDUAPP:" + OPENVIDU_SERVER_SECRET),
           "Content-Type": "application/json",
         },
       })
@@ -28,12 +69,12 @@ export function createSession(sessionId: string): Promise<any> {
       .catch((response) => {
         var error = Object.assign({}, response);
         if (error?.response?.status === 409) {
-          resolve(sessionId);
+          resolve(liveId);
         } else {
           console.log(error);
           console.warn(
             "No connection to OpenVidu Server. This may be a certificate error at " +
-              OPENVIDU_SERVER_URL
+            OPENVIDU_SERVER_URL,
           );
           // window.location.assign(OPENVIDU_SERVER_URL + "accept-certificate");
         }
@@ -41,17 +82,14 @@ export function createSession(sessionId: string): Promise<any> {
   });
 }
 
-export function createToken(liveId: string): Promise<any> {
+function createToken(liveId: string): Promise<any> {
   return new Promise((resolve, reject) => {
     const data = {};
     axios
       .post(
-        OPENVIDU_SERVER_URL + "openvidu/api/sessions/" + liveId + "/connection",
-        data,
-        {
+        OPENVIDU_SERVER_URL + "openvidu/api/sessions/" + liveId + "/connection", data, {
           headers: {
-            Authorization:
-              "Basic " + btoa("OPENVIDUAPP:" + OPENVIDU_SERVER_SECRET),
+            Authorization: "Basic " + btoa("OPENVIDUAPP:" + OPENVIDU_SERVER_SECRET),
             "Content-Type": "application/json",
           },
         }
--- FrontEnd/src/shared/common-data.tsx
+++ FrontEnd/src/shared/common-data.tsx
@@ -1,1 +1,1 @@
-export const api = "http://i9a805.p.ssafy.io:8090";
+export const api = "https://www.tcha.site/api";

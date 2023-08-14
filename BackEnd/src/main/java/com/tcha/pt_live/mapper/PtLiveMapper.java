3d0b387d7cf366696650f356574290d1e8459a9e
chogoal <goal961@naver.com>
2023-08-14 15:26:25 +0900

    fix: pt 라이브 컨트롤러에 getOnePtLive 추가

--- BackEnd/src/main/java/com/tcha/pt_live/mapper/PtLiveMapper.java
+++ BackEnd/src/main/java/com/tcha/pt_live/mapper/PtLiveMapper.java
@@ -1,25 +1,27 @@
 package com.tcha.pt_live.mapper;
 
-import com.tcha.pt_class.entity.PtClass;
 import com.tcha.pt_live.dto.PtLiveDto;
 import com.tcha.pt_live.entity.PtLive;
+import com.tcha.trainer.entity.Trainer;
+import com.tcha.user_profile.entity.UserProfile;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface PtLiveMapper {
 
-//    default PtLiveDto.Response ptLiveToResponseDto(PtLive ptLive, PtClass ptClass) {
-//        return PtLiveDto.Response.builder()
-//                .ptLiveId(ptLive.getId())
-//                .ptClassId(ptClass.getId())
-//                .startAt(ptClass.getStartAt())
-//                .closeAt(ptClass.getCloseAt())
-////                .price(ptClass.getPrice())
-//                .price("7,000원")
-//                .trainerId(ptLive.getTrainerId())
-//                .trainerProfileImage(ptClass.getTrainer().getUserProfile().getProfileImage())
-//                .trainerName(ptClass.getTrainer().getUserProfile().getName())
-//                .build();
-//    }
+    default PtLiveDto.Response ptLiveToResponseDto(PtLive ptLive, Trainer trainer,
+            UserProfile userProfile) {
+        return PtLiveDto.Response.builder()
+                .ptLiveId(ptLive.getId())
+                .status(ptLive.getStatus().toString())
+                .ptClassId(ptLive.getPtClassId())
+                .trainerId(trainer.getId())
+                .trainerName(trainer.getUserProfile().getName())
+                .trainerProfileImage(trainer.getUserProfile().getProfileImage())
+                .userId(userProfile.getUser().getId())
+                .userName(userProfile.getName())
+                .userProfileImage(userProfile.getProfileImage())
+                .build();
+    }
 
 }

38b71ea62c213cde6b234a9ea167203a447edcbf
chogoal <goal961@naver.com>
2023-08-17 16:00:20 +0900

    fix: 트레이너 캘린더 화면 수정

--- FrontEnd/src/containers/user-trainer/trainer-schedule.tsx
+++ FrontEnd/src/containers/user-trainer/trainer-schedule.tsx
@@ -8,69 +8,97 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 import dayjs, { Dayjs } from "dayjs";
-import { Link, useNavigate } from "react-router-dom";
 import { TrainerScheduleData } from "src/interface";
 import { GrayButton, TchaButton } from "@shared/button";
 import WriteExerciseLog from "@user-trainer/write-exercise-log";
-
-
-const CalendarContainer = styled.div`
-  background-color: white;
-  border-radius: 10px;
-  width: 100%;
-  display: flex;
-  align-items: center;
-`;
+import TrainerScheduleItem from "../trainer-schedule/trainer-schedule-item";
+import CreateClasses from "./create-classes";
+import { TitleWrapper, PageTitleText } from "@shared/page-title";
+import { useNavigate } from "react-router-dom";
+import { alignProperty } from "@mui/material/styles/cssUtils";
+import { display, margin } from "@mui/system";
 
 const Wrapper = styled.div`
   display: flex;
+  width: 96%;
   flex-direction: column;
-  height: 100vh;
-  margin: 1% 1%;
-`;
-
-const PageTitle = styled.div`
-  display: flex;
-  height: 10%;
-  align-items: center;
-  padding-left: 2%;
-  font-size: 2rem;
+  min-height: 100vh;
   margin: 1% 0%;
 `;
-const Calendar = styled.div`
-  margin-top: 3%;
+
+const ContentsWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  @media (max-width: 767px) {
+    flex-direction: column;
+  }
 `;
+
+const CalendarWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  /* align-items: center; */
+  margin-right: 2%;
+  width: 40%;
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.color.light};
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1);
+  @media (max-width: 767px) {
+    width: 100%;
+    max-width: 100%;
+    margin-right: 0%;
+    justify-content: center;
+  }
+`;
+
+const CalendarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #ebebeb;
+  border-radius: 10px;
+  width: 20rem;
+  height: 21rem;
+  margin-top: 5%;
+  /* padding: 1% 0%; */
+  /* box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1); */
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.1);
+  @media (max-width: 767px) {
+    margin-top: 3%;
+    background-color: ${({ theme }) => theme.color.light};
+    align-items: center;
+    justify-content: center;
+    width: 90%;
+    height: 90%;
+  }
+`;
+
 const ScheduleInfo = styled.div`
   display: flex;
-  flex-direction: row;
-  margin-top: 5%;
-  padding: 2%;
+  flex-direction: column;
+  align-items: center;
+  width: 70%;
+  min-height: 40rem;
+  padding: 1% 0%;
   border-radius: 10px;
-  background-color: #fff;
+  background-color: ${({ theme }) => theme.color.light};
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1);
+  @media (max-width: 767px) {
+    width: 100%;
+    padding: 2% 0%;
+    min-height: 40%;
+    margin-top: 3%;
+    box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.1);
+  }
 `;
 
-export interface PtLiveData {
-  ptClassId: number;
-  ptLiveId: number;
-  status: string;
-  trainerId: string;
-  trainerName: string;
-  trainerProfileImage: string;
-  userId: string;
-  userName: string;
-  userProfileImage: string;
-}
-
 function TrainerSchedule() {
-  const [value, setValue] = useState<Dayjs | null>(dayjs());
+  const [date, setDate] = useState<Dayjs | null>(dayjs());
   const trainer = useSelector((state: RootState) => state.profile.trainerId);
-  const selectedDate = value?.format("YYYY-MM-DD");
+  const selectedDate = date?.format("YYYY-MM-DD");
   const [items, setItems] = useState<TrainerScheduleData[]>([]);
 
   const navigate = useNavigate();
-
-  // 오픈비두
-  const [ptLiveData, setPtLiveData] = useState<PtLiveData>();
 
   useEffect(() => {
     axios
@@ -84,65 +112,43 @@ function TrainerSchedule() {
       });
   }, [trainer]);
 
-
-  const goToPtRoom = (ptLiveId: number) => {
-    axios
-      .get(`${api}/lives/${ptLiveId}`)
-      .then((response) => {
-        const ptLiveData:PtLiveData = response.data;
-
-        // 트레이너 입장 가능 여부 확인
-        if (ptLiveData.trainerId === trainer) {
-          // if (ptLiveData?.status === "PROGRESS") {
-          //   navigate("/pt", { state: ptLiveData });
-          // } else {
-          //   alert("입장 가능한 시간이 아닙니다");
-          // }
-          navigate("/pt", { state: ptLiveData });
-        } else {
-          alert("입장 권한이 없습니다");
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
   return (
     <Wrapper>
-      <PageTitle>PT 수업 일정</PageTitle>
-      <Link to="create_classes">수업 일정 생성하러가기</Link>
-      <Calendar>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <CalendarContainer>
-            <DateCalendar
-              value={value}
-              onChange={(selected: Dayjs | null) => setValue(selected)}
-            />
-          </CalendarContainer>
-        </LocalizationProvider>
-      </Calendar>
-      {items.map(
-        (item, index) =>
-          item.startDate === selectedDate && (
-            <ScheduleInfo key={index}>
-              <div>
-                Class Id : {item.classId} Live Id : {item.liveId} Date :{" "}
-                {item.startDate} Time : {item.startTime}
-                Trainer ID : {item.trainerId}
-              </div>
-              <TchaButton
-                onClick={() => goToPtRoom(item.liveId)}
-                style={{ width: "8rem", color: "white" }}
-              >
-                PT 입장하기
-              </TchaButton>
-              {item.liveId && item.status !== "INACCESSABLE" && (
-                <WriteExerciseLog liveId={item.liveId} />
-              )}
-            </ScheduleInfo>
-          )
-      )}
+      <TitleWrapper>
+        <PageTitleText>PT 수업 일정</PageTitleText>
+      </TitleWrapper>
+      <ContentsWrapper>
+        <CalendarWrapper>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <CalendarContainer>
+              <DateCalendar
+                value={date}
+                onChange={(selected: Dayjs | null) => setDate(selected)}
+              />
+            </CalendarContainer>
+          </LocalizationProvider>
+          <TchaButton
+            onClick={() => navigate("create_classes")}
+            style={{ color: "white", height: "3rem" }}
+          >
+            수업 일정 생성하기
+          </TchaButton>
+        </CalendarWrapper>
+        <ScheduleInfo>
+          {items.length > 0 ? (
+            items.map((item) => {
+              if (item.startDate === selectedDate) {
+                return <TrainerScheduleItem data={item} />;
+              }
+              return null;
+            })
+          ) : (
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <p>해당 날짜에 예약이 없습니다</p>
+            </div>
+          )}
+        </ScheduleInfo>
+      </ContentsWrapper>
     </Wrapper>
   );
 }
